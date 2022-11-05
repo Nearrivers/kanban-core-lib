a33e0454354d8271d4dc8b22b29e3e6e4a35c689
Antoine <a.fourcade65@gmail.com>
2022-11-05 12:36:13 +0100

    feat(entity): added field validation on list entity

--- src/entities/List.ts
+++ src/entities/List.ts
@@ -1,3 +1,4 @@
+import { IsInt, Length, Max, Min } from "class-validator";
 import { Column, Entity, ManyToOne, OneToMany, Relation } from "typeorm";
 import { GenericEntity } from "../utils/GenericEntity";
 import { Project } from "./Project";
@@ -5,12 +6,14 @@ import { Task } from "./Task";
 
 @Entity()
 export class List extends GenericEntity {
-    @Column({
-        length: 50
-    })
+    @Column()
+    @Length(50)
     name: string;
 
     @Column()
+    @IsInt()
+    @Min(0)
+    @Max(16777215)
     color: number;
 
     @ManyToOne(() => Project, (project) => project.lists)
