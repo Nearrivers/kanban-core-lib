3735e008c4bbcaff4d666d4be02f084ed31e08ab
Antoine <a.fourcade65@gmail.com>
2022-11-05 12:56:27 +0100

    feat(entity): add field validation to tag entity

--- src/entities/Tag.ts
+++ src/entities/Tag.ts
@@ -2,15 +2,18 @@ import { Column, Entity, ManyToMany, Relation } from "typeorm";
 import { Project } from './Project';
 import { GenericEntity } from '../utils/GenericEntity';
 import { Task } from './Task';
+import { IsInt, Length, Max, Min } from "class-validator";
 
 @Entity()
 export class Tag extends GenericEntity {
-    @Column({
-        length: 50
-    })
+    @Column()
+    @Length(50)
     name: string;
 
     @Column()
+    @IsInt()
+    @Min(0x0000000)
+    @Max(0xFFFFFFF)
     color: number;
 
     @ManyToMany(() => Project, (project) => project.tags)
