06e58f3969fd671676af6592194963a6ab2a1870
Antoine <a.fourcade65@gmail.com>
2022-11-06 10:54:35 +0100

    refactor(list): removed useless validators

--- src/entities/List.ts
+++ src/entities/List.ts
@@ -1,4 +1,4 @@
-import { IsDefined, isDefined, IsInt, IsNotEmpty, isNotEmpty, Length, Max, Min } from "class-validator";
+import { IsInt, Length, Max, Min } from "class-validator";
 import { Column, Entity, ManyToOne, OneToMany, Relation } from "typeorm";
 import { GenericEntity } from "../utils/GenericEntity";
 import { Project } from "./Project";
@@ -7,24 +7,20 @@ import { Task } from "./Task";
 @Entity()
 export class List extends GenericEntity {
     @Column()
-    @Length(50)
-    @IsDefined()
-    @IsNotEmpty()
+    @Length(1, 50)
     name: string;
 
     @Column()
     @IsInt()
     @Min(0x0000000)
     @Max(0xFFFFFFF)
-    @IsDefined()
-    @IsNotEmpty()
     color: number;
 
     @ManyToOne(() => Project, (project) => project.lists)
     project: Relation<Project>;
 
     @OneToMany(() => Task, (task) => task.list, {
-        cascade: true
+        cascade: ['remove']
     })
     tasks?: Relation<Task[]>;
 }
