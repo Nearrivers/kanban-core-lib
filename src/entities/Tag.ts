ce0f63ba70378917c907702e5cf4195ceead70c3
Antoine <a.fourcade65@gmail.com>
2022-11-06 10:56:05 +0100

    refactor(tag): removed useless validators

--- src/entities/Tag.ts
+++ src/entities/Tag.ts
@@ -2,22 +2,18 @@ import { Column, Entity, ManyToMany, Relation } from "typeorm";
 import { Project } from './Project';
 import { GenericEntity } from '../utils/GenericEntity';
 import { Task } from './Task';
-import { IsDefined, IsInt, IsNotEmpty, Length, Max, Min } from "class-validator";
+import { IsInt, Length, Max, Min } from "class-validator";
 
 @Entity()
 export class Tag extends GenericEntity {
     @Column()
-    @Length(50)
-    @IsDefined()
-    @IsNotEmpty()
+    @Length(1, 50)
     name: string;
 
     @Column()
     @IsInt()
     @Min(0x0000000)
     @Max(0xFFFFFFF)
-    @IsDefined()
-    @IsNotEmpty()
     color: number;
 
     @ManyToMany(() => Project, (project) => project.tags)
