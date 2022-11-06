13458dfb0be3298645eec0468c30750355513fa5
Antoine <a.fourcade65@gmail.com>
2022-11-06 11:59:23 +0100

    test(user): user entity tests

---
+++ src/entities/__tests__/user.spec.ts
@@ -0,0 +1,74 @@
+import { validate } from 'class-validator';
+import { EntityManager } from 'typeorm';
+import { MockConnection } from '../../utils/MockConnection';
+import { User } from '../User';
+
+describe('user entity tests', () => {
+  const mockConnection = new MockConnection();
+  let manager: EntityManager;
+
+  beforeAll(async () => {
+    manager = await mockConnection.initializeMockDb();
+  })
+
+  test('GIVEN I want to create a new user '
+    + 'WHEN I try to insert an user with the correct fields '
+    + 'THEN a new user is created '
+    + 'AND I am able to retrieve it', async () => {
+      const user = new User();
+      user.name = 'Antoine';
+
+      await manager.save(user);
+
+      const createdUser = await manager.findOne(User, {
+        where: {
+          name: 'Antoine'
+        }
+      });
+
+      expect(createdUser.name).toEqual('Antoine');
+    })
+
+  test('GIVEN I want to create a new user '
+    + 'WHEN I try to insert an user with an undefined name '
+    + 'THEN no user is created', async () => {
+      const user = new User();
+      user.name = undefined;
+
+      const errors = await validate(user);
+
+      if (errors.length > 0) {
+        expect(errors[0].constraints.isLength).toEqual('name must be longer than or equal to 2 characters');
+      }
+    })
+
+  test('GIVEN I want to create a new user '
+    + 'WHEN I try to insert an user with an empty name '
+    + 'THEN no user is created', async () => {
+      const user = new User();
+      user.name = '';
+
+      const errors = await validate(user);
+
+      if (errors.length > 0) {
+        expect(errors[0].constraints.isLength).toEqual('name must be longer than or equal to 2 characters');
+      }
+    })
+
+  test('GIVEN I want to create a new user '
+    + 'WHEN I try to insert an user with a name that is too long '
+    + 'THEN no user is created', async () => {
+      const user = new User();
+      user.name = 'ABCCDEFGHIJKLMNOPQRSTUVWXYZABCCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+      const errors = await validate(user);
+
+      if (errors.length > 0) {
+        expect(errors[0].constraints.isLength).toEqual('name must be shorter than or equal to 50 characters');
+      }
+    })
+
+  afterAll(async () => {
+    await mockConnection.tearDown();
+  })
+})
