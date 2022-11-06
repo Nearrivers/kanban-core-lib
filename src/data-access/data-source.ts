f63f3ff6a6ce6a774c19c5896b8895850871f165
Antoine <a.fourcade65@gmail.com>
2022-11-06 10:53:33 +0100

    feat(entities): add a glob pattern to read entities

--- src/data-access/data-source.ts
+++ src/data-access/data-source.ts
@@ -1,17 +1,22 @@
+import path from 'path';
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
-// import { User } from "./entity/User"
 // import dotenv from 'dotenv';
 
 // dotenv.config();
+
+const entityPaths = [
+  path.join(__dirname, './../entities/*.ts'),
+  // pour la prod
+  path.join(__dirname, './../entities/*.js')
+];
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
   database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
-  // TODO: add a glob pattern to read entities
-  // entities: [User],
+  entities: entityPaths,
   migrations: [],
   subscribers: [],
 })
